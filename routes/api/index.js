160ac220edf1bf53ca4e0819131171a53d59d72b
Katie Buchholz <kzbindy@hotmail.com>
2024-07-16 15:55:59 -0400

    working on the back-end, so the submit button would be create new form data after the user fills it out

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,3 +2,6 @@
 // router, and getting express
 
 // direct files to route files (bidder routes, and donation routes)
+const router = require('express').Router()
+
+const bidRoutes = require('bidder-routes.js')
